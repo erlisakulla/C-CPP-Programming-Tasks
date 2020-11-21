fbe8a69174345f4e2ab77fdb9003c9c14495e261
erlisakulla <68386106+erlisakulla@users.noreply.github.com>
2020-11-21 18:16:01 +0100

    Create a1p1.c

---
+++ a1/a1p1.c
@@ -0,0 +1,15 @@
+/*
+CH-230-A
+a1p2.c
+Erlisa Kulla
+e.kulla@jacobs-university.de
+*/
+
+#include <stdio.h>
+
+int main() {
+	int result; /* The result of our calculation */
+	result = (2 + 7) * 9 / 3;
+	printf("The result is %d\n", result); 
+	return 0;
+}
